833f4cea5da78c40811aaa313fcb71abaa0b2b07
Roberto Martins <roberto.martins.info@gmail.com>
2019-02-28 00:38:40 -0300

    Exemplo de tabela para implementação em usuarios.

--- frontend/src/pages/usuario/lista/Lista.js
+++ frontend/src/pages/usuario/lista/Lista.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
 import './UsuarioLista.scss';
+import { Table, Container } from 'semantic-ui-react';
 
 class UsuarioLista extends Component {
-  render() {
-    return (
-      <div className="App">
-        Lista de usuários.
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Container style={{ marginTop: '7em' }}>
+                <Table celled inverted selectable>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>ID</Table.HeaderCell>
+                            <Table.HeaderCell>Data de cadastro</Table.HeaderCell>
+                            <Table.HeaderCell>Telefone</Table.HeaderCell>
+                            <Table.HeaderCell>Nome</Table.HeaderCell>
+                            <Table.HeaderCell>Sobrenome</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+
+                    <Table.Body>
+                        <Table.Row>
+                            <Table.Cell>usuario</Table.Cell>
+                            <Table.Cell>12/12/2019</Table.Cell>
+                            <Table.Cell textAlign='right'>(51) 99602-7850</Table.Cell>
+                            <Table.Cell>Roberto</Table.Cell>
+                            <Table.Cell>Martins</Table.Cell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.Cell>usuario</Table.Cell>
+                            <Table.Cell>12/12/2019</Table.Cell>
+                            <Table.Cell textAlign='right'>(51) 99602-7850</Table.Cell>
+                            <Table.Cell>Roberto</Table.Cell>
+                            <Table.Cell>Martins</Table.Cell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.Cell>usuario</Table.Cell>
+                            <Table.Cell>12/12/2019</Table.Cell>
+                            <Table.Cell textAlign='right'>(51) 99602-7850</Table.Cell>
+                            <Table.Cell>Roberto</Table.Cell>
+                            <Table.Cell>Martins</Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+                </Table>
+            </Container>
+        );
+    }
 }
 
 export default UsuarioLista;
