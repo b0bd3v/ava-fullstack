1edd935c6b3056306d7deeb1bf2e3a7755698603
Roberto Martins <roberto.martins.info@gmail.com>
2019-02-28 00:29:30 -0300

    Ajuste de botões de troca de rota. Implementação de redirect.

--- frontend/src/header/Header.js
+++ frontend/src/header/Header.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import './Header.scss';
 import { Menu, Container, Image } from 'semantic-ui-react';
+import { Redirect } from 'react-router-dom';
+
 
 class Header extends Component {
+    
+    state = {}
+
+    handlerMenuClick = (e, {name}) => {
+        this.setState({
+            itemAtivo: name,
+            mudouRota: true,
+            novaRota: '/' + name
+        })
+        
+    }
+
     render() {
+        const { itemAtivo } = this.state 
+        
+        if(this.state.mudouRota === true){
+            this.setState({
+                mudouRota: false
+            })
+            return <Redirect to={this.state.novaRota} />
+        }
+        
         return (
             <Menu fixed='top' inverted>
                 <Container>
@@ -12,9 +35,9 @@ class Header extends Component {
                         <Image size='mini' src={logo} style={{ marginRight: '1.5em' }} />
                         Controle usuários
                     </Menu.Item>
-                    <Menu.Item as='a'>Home</Menu.Item>
-                    <Menu.Item as='a'>Usuários</Menu.Item>
-                    <Menu.Item as='a'>Sobre</Menu.Item>
+                    <Menu.Item as='a' name="home" active={itemAtivo === 'home'} onClick={this.handlerMenuClick}>Home</Menu.Item>
+                    <Menu.Item as='a' name="usuarios" active={itemAtivo === 'usuarios'} onClick={this.handlerMenuClick}>Usuários</Menu.Item>
+                    <Menu.Item as='a' name="sobre" active={itemAtivo === 'sobre'} onClick={this.handlerMenuClick}>Sobre</Menu.Item>
                 </Container>
             </Menu>
         );
