6c44f12c0188db786c46760b185c04bcba9ca411
Roberto Martins <roberto.martins.info@gmail.com>
2019-02-27 23:25:40 -0300

    Estrutura inicial de rotas.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Sobre from './pages/sobre/Sobre';
+import UsuarioLista from './pages/usuario/lista/Lista';
+import Header from './header/Header';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(
+    [<Header />, <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={App} />
+            <Route path="/sobre" exact={true} component={Sobre} />
+            <Route path="/usuarios" exact={true} component={UsuarioLista} />
+        </Switch>
+    </BrowserRouter>]
+    , document.getElementById('root'));
 serviceWorker.unregister();
