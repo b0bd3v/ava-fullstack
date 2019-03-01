53dcfce85c1d038f24b8c3b29a964bcc9509889d
Roberto Martins <roberto.martins.info@gmail.com>
2019-03-01 14:06:29 -0300

    Criação de ação de update de usuário.

--- backend/app/Controllers/Http/UsuarioController.js
+++ backend/app/Controllers/Http/UsuarioController.js
@@ -2,9 +2,24 @@
 const User = use('App/Models/User')
 
 class UsuarioController {
-    index(resquest, response){
+    async index(resquest, response){
         const usuarios = User.all()
         return usuarios
+    }
+
+    async show({params}){
+        const usuario = User.find(params)
+        return usuarios
+    }
+    
+    async update(params, request, response){
+        console.log(params, request, response);
+        
+        // const usuario = User.where({ _id: params._id }).update(
+        //     params            
+        // )
+
+        // return usuario
     }
 }
 
