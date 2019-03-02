c8e632cf386916190d00ab7efa3463dbfe9bfa7c
Roberto Martins <roberto.martins.info@gmail.com>
2019-03-01 21:04:20 -0300

    Ajuste de rotas

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Sobre from './pages/sobre/Sobre';
 import UsuarioLista from './pages/usuario/lista/UsuarioLista';
 import Header from './header/Header';
 
@@ -16,7 +15,6 @@ ReactDOM.render(
         <Switch>
             <Route path="/" exact={true} component={App} />
             <Route path="/home" exact={true} component={App} />
-            <Route path="/sobre" exact={true} component={Sobre} />
             <Route path="/usuarios" exact={true} component={UsuarioLista} />
         </Switch>
         </div>
